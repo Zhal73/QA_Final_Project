74e17d9edd037fb82df519c09edd3c48741e4e97
Ed Prickett <68053844+omris-elda@users.noreply.github.com>
2020-09-22 15:57:47 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,15 +4,13 @@ sudo groupadd -f docker
 sudo usermod -aG docker $(whoami)
 sudo chmod 666 /var/run/docker.sock
 
-export DB_PASSWORD="petclinic"
 echo $DB_PASSWORD
-export DB_URI="jdbc:mysql://34.89.75.156:3306/petclinic?useUnicode=true"
 echo $DB_URI
 
-sudo docker login
-sudo docker-compose build
-sudo docker push qajulyteam2/pet-clinic-client:latest
-sudo docker push qajulyteam2/pet-clinic-rest:latest
+docker login
+docker-compose build
+docker push qajulyteam2/pet-clinic-client:latest
+docker push qajulyteam2/pet-clinic-rest:latest
 
 cd kubernetes
 ./start.sh
