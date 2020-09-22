72716f2cef9e49f71989f38e3d29b476a798ea5f
Ed Prickett <68053844+omris-elda@users.noreply.github.com>
2020-09-22 15:36:09 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,11 @@ sudo groupadd -f docker
 sudo usermod -aG docker $(whoami)
 sudo chmod 666 /var/run/docker.sock
 
+export DB_PASSWORD="petclinic"
+echo $DB_PASSWORD
+export DB_URI="jdbc:mysql://34.89.75.156:3306/petclinic?useUnicode=true"
+echo $DB_URI
+
 sudo docker login
 sudo docker-compose build
 sudo docker push qajulyteam2/pet-clinic-client:latest
