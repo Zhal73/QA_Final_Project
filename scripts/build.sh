79ec7ecfdfcb9550303d785be552a290a095aa4d
Sithembiso Ngwenya <66832040+ngww@users.noreply.github.com>
2020-09-22 14:40:35 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,8 +6,8 @@ sudo chmod 666 /var/run/docker.sock
 
 sudo docker login
 sudo docker-compose build
-sudo docker push qajulyteam2/spring-petclinic-angular:latest
-sudo docker push qajulyteam2/spring-petclinic-rest:latest
+sudo docker push qajulyteam2/pet-clinic-client:latest
+sudo docker push qajulyteam2/pet-clinic-rest:latest
 
 cd kubernetes
 ./start.sh
