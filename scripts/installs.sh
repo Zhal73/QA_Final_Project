ec22d6c645eddbcd3c462a3ae3627ae8b2f0450d
ngww <ngww95@gmail.com>
2020-09-22 07:52:36 +0000

    Jenkins

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -28,21 +28,3 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 # installs maven
 sudo apt install -y maven
-
-# installs angular-cli
-sudo apt install -y npm
-sudo npm uninstall -g angular-cli @angular/cli
-sudo npm cache clean
-sudo npm install -g @angular/cli@latest
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt install -y nodejs
-
-sudo npm install --save-dev @angular/cli@latest
-sudo npm install -y
-
-# installs Karma
-sudo npm install karma
-
-# installs Protractor
-sudo npm install -g protractor
-sudo webdriver-manager update
--- scripts/testing.sh
+++ scripts/testing.sh
@@ -1,8 +1,3 @@
 #! /bin/bash
 
-ng test
-
-# end-to-end tests via Protractor
-sudo webdriver-manager start
-ng serve
-ng e2e
+mvn test
