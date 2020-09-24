dd7ff11c763f04a0280cbef2f0794c0f222b6146
domenico <domenico.gagliano1973@gmail.com>
2020-09-24 11:42:18 +0000

    enviroment variables

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sed 's/{{DB_URI}}/${DB_URI}/g;s/{{DB_PASSWORD}}/${DB_PASSWORD}/g' secret.yaml | kubectl apply -f -
 kubectl apply -f config-map.yaml
 kubectl apply -f backend.yaml
 kubectl apply -f frontend.yaml
