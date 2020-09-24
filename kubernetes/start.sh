9d072662f351873f2a17a4e1ffd84098f20c504c
Ed Prickett <68053844+omris-elda@users.noreply.github.com>
2020-09-24 14:39:59 +0100

    Update start.sh

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sed 's/{{dburi}}/${DB_URI}/g;s/{{dbpassword}}/${DB_PASSWORD}/g' secret.yaml | kubectl apply -f -
+sed 's/{{db_uri}}/${dburi}/g;s/{{db_password}}/${dbpassword}/g' secret.yaml | kubectl apply -f -
 kubectl apply -f config-map.yaml
 kubectl apply -f backend.yaml
 kubectl apply -f frontend.yaml
